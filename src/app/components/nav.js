d510496abc43e2c4d21ebeb8d85e2f30e93f9fca
Tyler Hochrine <tylerjhochrine@gmail.com>
2023-06-18 20:18:07 -0400

    Init

---
+++ src/app/components/nav.js
@@ -0,0 +1,51 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import NavItem from "./nav_item.js";
+
+const MENU_LIST = [
+  { text: "Home", href: "/" },
+  { text: "Keepers", href: "/keepers" },
+  { text: "History", href: "/history" },
+  { text: "Accolades", href: "/accolades" },
+];
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
+  return (
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"} legacyBehavior>
+          <a>
+            <h1 className="logo">Fantasy Civil War</h1>
+          </a>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
